bdc26b2cc8345f49a9c2cb7b8a04babdf018af89
DANIEL886886 <danielparaschiv886@gmail.com>
2024-06-01 15:45:30 +0300

    update Spint10-JS-in browser

--- ex1/Tema3-V.4-Daniel Paraschiv.js
+++ ex1/Tema3-V.4-Daniel Paraschiv.js
@@ -1,35 +1,35 @@
 document.addEventListener("DOMContentLoaded", () => {
 
-       //it worked only after i added the DOMcontentloader event and after I initialized both form and button and changed the button type from submit to click.
-       const form = document.getElementById("contact-form");
-       const button = document.getElementById("button");
+    //it worked only after i added the DOMcontentloader event and after I initialized both form and button and changed the button type from submit to click.
+    const form = document.getElementById("contact-form");
+    const button = document.getElementById("button");
 
-button.addEventListener("click", (e) => {
-         
-       // this line is to prevent the form to load twice
-       e.preventDefault();
-         
-       // constants declaration : name, email, text, formData
-       const formData = new FormData(form);
-       const nume = formData.get("nume")
-       const email = formData.get("email")
-       const text = formData.get("text")
-       
-       // alert message with ok button
-       alert("\n" + "\n" + "Un email la adresa: " + email + " a fost trimis." + "\n" + "\n" + "Va multumesc !");
+    button.addEventListener("click", (e) => {
+            
+        // this line is to prevent the form to load twice
+        e.preventDefault();
+        
+        // constants declaration : name, email, text, formData
+        const formData = new FormData(form);
+        const nume = formData.get("nume")
+        const email = formData.get("email")
+        const text = formData.get("text")
+        
+        // alert message with ok button
+        alert("\n" + "\n" + "Un email la adresa: " + email + " a fost trimis." + "\n" + "\n" + "Va multumesc !");
 
-       // print into the console the form information  
-       console.log("This is the information extracted from the user-form: ")
-       console.log(`Nume: ${nume}`);
-       console.log(`Email: ${email}`);
-       console.log(`Mesaj: ${text}`);
+        // print into the console the form information  
+        console.log("This is the information extracted from the user-form: ")
+        console.log(`Nume: ${nume}`);
+        console.log(`Email: ${email}`);
+        console.log(`Mesaj: ${text}`);
 
-       // empty the input cell boxes after the information was printed on the console
-       const inputs = form.querySelectorAll('input');
-              inputs.forEach(input => input.value = '');
-       const textarea = form.querySelectorAll('textarea');
-              textarea.forEach(textarea=> textarea.value = '');
-  });
+        // empty the input cell boxes after the information was printed on the console
+        const inputs = form.querySelectorAll('input');
+                inputs.forEach(input => input.value = '');
+        const textarea = form.querySelectorAll('textarea');
+                textarea.forEach(textarea=> textarea.value = '');
+    });
 });
 
 // The only error is that i need to press twice the ok button from the alarm message, for the message to dissapear.
